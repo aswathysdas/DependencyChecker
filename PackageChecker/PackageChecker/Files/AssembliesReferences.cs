46df0a7d4ac6d2238ef7a91a61e91908bd1b0d4d
Chuck Lu <chuck.lu@qq.com>
2020-11-27 17:40:46 +0800

    check the bindingRedirect according the configuration file path

--- PackageChecker/PackageChecker/Files/AssembliesReferences.cs
+++ PackageChecker/PackageChecker/Files/AssembliesReferences.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Configuration;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace PackageChecker.Files
 {
@@ -61,11 +64,62 @@ namespace PackageChecker.Files
             bool exist = !(assembliesInFolder.Contains(refAssemblyFullName) ||
                            additionalAssemblies.Contains(refAssemblyFullName)) &&
                          !AssemblyManager.IsAssemblyInGAC(referencedAssembly);
+            if (exist)
+            {
+                return true;
+            }
 
+            //check bindingRedirect here
+            var bindingRedirectFullName = GetBindingRedirectFullName(referencedAssembly.Name);
+            var bindingRedirectAssembly = new AssemblyName(bindingRedirectFullName);
+            exist = !(assembliesInFolder.Contains(bindingRedirectFullName) ||
+                      additionalAssemblies.Contains(bindingRedirectFullName)) &&
+                    !AssemblyManager.IsAssemblyInGAC(bindingRedirectAssembly);
             return exist;
         }
 
-        private HashSet<string> GetAdditionalAssemblies(string folder, Assembly assembly)
+        private string GetBindingRedirectFullName(string assemblyName)
+        {
+            var filePath = ConfigurationManager.AppSettings["ConfigFileName"];
+            var root = XElement.Load(filePath);
+            var root2 = RemoveAllNamespaces(root);
+            var runtime = root2.Element("runtime");
+            var assemblyBinding = runtime?.Element("assemblyBinding");
+            var assemblyIdentity = assemblyBinding?.Elements("dependentAssembly")
+                .Elements("assemblyIdentity").FirstOrDefault(x => x.Attribute("name")?.Value == assemblyName);
+            var dependentAssembly = assemblyIdentity?.Parent;
+            var bindingRedirect = dependentAssembly?.Elements("bindingRedirect").FirstOrDefault();
+            var newVersion = bindingRedirect?.Attribute("newVersion")?.Value;
+            //Console.WriteLine(newVersion);
+            var culture = assemblyIdentity?.Attribute("culture")?.Value;
+            var publicKeyToken = assemblyIdentity?.Attribute("publicKeyToken")?.Value;
+            var bindingRedirectFullName =
+                $"{assemblyName}, Version={newVersion}, Culture={culture}, PublicKeyToken={publicKeyToken?.ToLower()}";
+            //Console.WriteLine(bindingRedirectFullName);
+            return bindingRedirectFullName;
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/a/988325/13338936
+        /// </summary>
+        /// <param name="xmlDocument"></param>
+        /// <returns></returns>
+        private static XElement RemoveAllNamespaces(XElement xmlDocument)
+        {
+            if (!xmlDocument.HasElements)
+            {
+                XElement xElement = new XElement(xmlDocument.Name.LocalName);
+                xElement.Value = xmlDocument.Value;
+
+                foreach (XAttribute attribute in xmlDocument.Attributes())
+                    xElement.Add(attribute);
+
+                return xElement;
+            }
+            return new XElement(xmlDocument.Name.LocalName, xmlDocument.Elements().Select(RemoveAllNamespaces));
+        }
+
+		private HashSet<string> GetAdditionalAssemblies(string folder, Assembly assembly)
 		{
 			HashSet<string> additionalAssemblies = new HashSet<string>();
 
