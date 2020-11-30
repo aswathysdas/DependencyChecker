146009d3432fb4dec5cff96ab19bf2ae323d9fd3
Chuck Lu <chuck.lu@qq.com>
2020-11-30 17:21:21 +0800

    show exception when load failed from GAC

--- PackageChecker/PackageChecker/Files/AssemblyManager.cs
+++ PackageChecker/PackageChecker/Files/AssemblyManager.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using PackageChecker.WindowManagement;
 
 namespace PackageChecker.Files
 {
@@ -42,8 +43,9 @@ namespace PackageChecker.Files
 			{
 				return GetAssemblyByName(assemblyName).GlobalAssemblyCache;
 			}
-			catch
+			catch(Exception ex)
 			{
+				WindowHelper.ShowError(ex.ToString());
 				return false;
 			}
 		}
